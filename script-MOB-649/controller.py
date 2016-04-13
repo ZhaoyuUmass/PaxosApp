2f2b8d7ed92b45f902737707ccd48d9692b1fe86
Zhaoyu Gao <gaozy@cs.umass.edu>
2016-04-13 17:04:21 -0400

    script is ready

---
+++ script-MOB-649/controller.py
@@ -0,0 +1,129 @@
+import paramiko
+import threading
+import SocketServer
+import time
+import datetime
+import subprocess
+import sys
+import os
+
+from threading import Condition
+
+KEY_FILE = "EC2"
+
+result = []
+cv = Condition()
+
+def loadHost():
+    dic = {}
+    fin = open("ec2-servers","r")
+    for line in fin:
+        line = line[:-1]
+        line = line.split(" ")
+        dic[line[0]] = line[1]
+    return dic
+
+hostToName = loadHost()
+
+
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        
+        print datetime.datetime.now(),"receive data:",self.data,"from ",self.request
+        global result
+        result.append(self.data)
+        cv.aquire()
+        cv.notify()
+        cv.release()
+
+
+class serverThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        HOST, PORT = "none.cs.umass.edu", NONE_PORT
+        print 'Start server at port '+str(PORT)
+        server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+        server.serve_forever()
+
+class cmdThread (threading.Thread):
+    def __init__(self, host, cmd):
+        threading.Thread.__init__(self)
+        self.host = host
+        self.cmd = cmd
+
+    def run(self):
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            ssh.connect(self.host, username = 'ubuntu', key_filename=KEY_FILE)
+            stdin, stdout, stderr = ssh.exec_command(self.cmd)
+            ssh.close()
+        except:
+            print 'Time out'
+
+
+# This function can be changed in the future, if the model to
+# start the client is changed
+def runClient(host, num_req):
+    cmd = "./PaxosEtherpad/ec2Client.sh etherpad.ReconfigurableEtherpadExpClient "
+    cmd += hostToName[host]
+    th = cmdThread(host, cmd)
+    th.start()
+
+
+def sendRequests(trace):
+    for load in trace:
+        host = load[0]
+        num_req = load[1]
+        runClient(host, num_req)
+        finished = False
+        cv.aquire()
+        while not finished:            
+            cv.wait()
+        cv.release()
+        print "this round is done for host "+host
+    print "Send all requests!"
+
+
+def loadTrace():
+    arr = []
+    fin = open("trace","r")
+    for line in fin:
+        line = line[:-1]
+        line = line.split(" ")
+        arr.append(line)
+    return arr
+
+def movingAverage(l):
+    return l
+
+def processResult():
+    yaxis = []
+    for item in result:
+        latencies = item.split(",")
+        yaxis = yaxis + latencies
+    xaxis = range(len(xaxis))
+    yaxis = movingAverage(yaxis)
+    print xaxis
+    print yaxis    
+        
+
+def main():
+    # Step 0: start server
+    serverThread().start()
+    
+    # Step 1: load trace
+    trace = loadTrace()
+    print trace
+    
+    # Step 2: send requests
+    sendRequests(trace)
+
+    # Step 3: plot data
+    processResult()
+
+if __name__ == "__main__":
+    main()
---
+++ src/etherpad/ReconfigurableEtherpadExpClient.java
@@ -0,0 +1,221 @@
+package etherpad;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.net.Socket;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.json.JSONException;
+
+import edu.umass.cs.gigapaxos.PaxosConfig;
+import edu.umass.cs.gigapaxos.interfaces.Request;
+import edu.umass.cs.gigapaxos.interfaces.RequestCallback;
+import edu.umass.cs.nio.interfaces.IntegerPacketType;
+import edu.umass.cs.reconfiguration.ReconfigurableAppClientAsync;
+import edu.umass.cs.reconfiguration.examples.AppRequest;
+import edu.umass.cs.reconfiguration.examples.noopsimple.NoopApp;
+import edu.umass.cs.reconfiguration.reconfigurationutils.RequestParseException;
+
+/**
+ * @author gaozy
+ *
+ */
+public class ReconfigurableEtherpadExpClient extends ReconfigurableAppClientAsync{
+	private static int NUM_REQ = 0;
+	private static String HOST_REGION = null;
+	private static String HOST_NAME = null;
+	// service name is also the pad name
+	private final static String serviceName = "ReconfigurableEtherpadApp0";
+	private final static int NUM_THREAD = 10;
+	private final static int TIMEOUT = 2000;
+	private final static int REQ_LENTGH = 130;
+	
+	private static ReconfigurableEtherpadExpClient client;	
+	private static ThreadPoolExecutor executorPool = new ThreadPoolExecutor(NUM_THREAD, NUM_THREAD, 0, TimeUnit.SECONDS, 
+    		new LinkedBlockingQueue<Runnable>(), new MyThreadFactory());
+	private static SecureRandom random = new SecureRandom();
+	
+	protected static String nextString() {
+	    return new BigInteger(REQ_LENTGH, random).toString(32);
+	}
+	
+	//static long totalLatency = 0;
+	static ArrayList<Long> latencies = new ArrayList<Long>();
+	static int received = 0;
+	static int timeout = 0;
+	static synchronized void updateLatency(long latency){
+		latencies.add(latency);
+	}
+	
+	 static class MyThreadFactory implements ThreadFactory {
+	    	
+	    	public Thread newThread(Runnable r) {
+	    		Thread t = new Thread(r);
+	    	    return t;
+	    	}
+	    }
+	
+	protected static class Callback implements RequestCallback{
+		
+		private long initTime;
+		private Object obj;
+		
+		Callback(long initTime, Object obj){
+			this.initTime = initTime;
+			this.obj = obj;
+		}
+		
+		@Override
+		public void handleResponse(Request response) {
+//			if(response.getRequestType()==ReconfigurationPacket.PacketType.ACTIVE_REPLICA_ERROR)
+//				return;
+			
+			long eclapsed = System.currentTimeMillis() - this.initTime;
+			
+			if(response.getRequestType() != AppRequest.PacketType.DEFAULT_APP_REQUEST){
+				System.out.println(response+" "+response.getRequestType());
+			}else{
+				updateLatency(eclapsed);
+				System.out.println("Latency of request"+received +":"+eclapsed+"ms");
+			}
+			synchronized(obj){
+				obj.notify();
+			}
+		}
+		
+	}
+	
+	private static class requestRunnable implements Callable<Boolean>{
+		Object obj = new Object();
+		
+		@Override
+		public Boolean call() throws Exception {
+			
+			try {
+				System.out.println("Send request "+received);
+				client.sendRequest(new AppRequest(serviceName, HOST_NAME+","+nextString(),
+						AppRequest.PacketType.DEFAULT_APP_REQUEST, false)
+						, new Callback(System.currentTimeMillis(), obj));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			synchronized(obj){
+				obj.wait();
+			}
+			return true;
+		}
+		
+	}
+	
+
+	
+	/**
+	 * @throws IOException
+	 */
+	public ReconfigurableEtherpadExpClient() throws IOException {
+		super();
+	}
+
+	public static void main(String[] args) throws IOException, InterruptedException{
+		//FIXME: remove this hard-coded part
+		String[] hosts = {"california", "virginia", "ireland", "sydney", "tokyo"};
+		HashMap<String, String> hostToName = new HashMap<String, String>();
+		
+		assert(hosts.length == PaxosConfig.getActives().keySet().size());
+		
+		boolean flag = false;
+		int i = 0;
+		for(String name:PaxosConfig.getActives().keySet()){
+			hostToName.put(hosts[i], name);
+			i++;
+		}
+		
+		if(args.length < 2){
+			System.out.println("Please enter the parameters as: #req host_region");
+			System.exit(0);
+		}else{
+			try{
+				NUM_REQ = Integer.parseInt(args[0]);
+			}catch(Exception e){
+				System.out.println("Please enter the parameter number of request as an integer.");
+				System.exit(0);
+			}
+			HOST_REGION = args[1].toLowerCase();
+			if(!hostToName.containsKey(HOST_REGION)){
+				System.out.println("Please make sure the parameter host region is one of "+hosts);
+				System.exit(0);
+			}
+			if(args.length == 3){
+				flag = Boolean.parseBoolean(args[2]);
+			}
+		}
+		
+		HOST_NAME = hostToName.get(HOST_REGION);
+		assert(HOST_NAME != null);
+		
+		client = new ReconfigurableEtherpadExpClient();
+		executorPool.prestartAllCoreThreads();
+		
+		while (received < NUM_REQ){
+			Future<Boolean> future = executorPool.submit(new requestRunnable());
+			try {
+				boolean rcvd = future.get(TIMEOUT, TimeUnit.MILLISECONDS);
+				if(rcvd){
+					received++;
+				}
+			} catch (ExecutionException | TimeoutException e) {
+				received++;
+				timeout++;
+				e.printStackTrace();
+			}
+		} 
+		long totalLatency = 0;
+		String response = "";
+		for (long lat:latencies){
+			totalLatency += lat;
+			response += lat+",";
+		}
+		response = response.substring(0, response.length()-2);
+		
+		System.out.println("Sent "+NUM_REQ+" requests, received "+(received-timeout)+" requests and "+ timeout + " requests timed out"
+				+ ". The average latency is "+totalLatency/(received-timeout)+"ms");
+		System.out.println(response);
+		
+		if(flag){
+			Socket socket = new Socket("128.119.245.5", 60001);
+	    	PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+	    	out.println(response+"\n");
+	    	out.flush();
+	    	socket.close();
+		}
+		
+	}
+
+	@Override
+	public Request getRequest(String stringified) throws RequestParseException {
+		try {
+			return NoopApp.staticGetRequest(stringified);
+		} catch (RequestParseException | JSONException e) {
+			// do nothing by design
+		}
+		return null;
+	}
+
+	@Override
+	public Set<IntegerPacketType> getRequestTypes() {
+		return NoopApp.staticGetRequestTypes();
+	}
+	
+}
